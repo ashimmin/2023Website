cfaa12e472e8f4430e5fc8433dec39c0cbe748af
ashimmin <ashimmin@cca.edu>
2023-08-24 16:32:22 +0000

    added portfolio content

--- app/design/page.js
+++ app/design/page.js
@@ -4,60 +4,60 @@ import Nav from "../components/nav";
 const designsData = [
   {
     title: "Ford",
-    link: "/",
+    link: "/ford",
     description:
       "Voluptate excepteur reprehenderit commodo. Minim dolor officia adipisicing minim cillum. Commodo Lorem et quis ipsum. Sit consequat aute duis Lorem commodo nisi elit sint irure. Nisi aliquip commodo elit dolore nostrud esse quis sit ea ea voluptate quis ipsum. Proident eiusmod nostrud fugiat est eiusmod. Ex dolor mollit consequat ut consectetur irure id minim ut sint cillum laborum veniam cupidatat. Excepteur laborum elit deserunt deserunt ullamco aute duis aute dolore.",
     image:
       "/assets/design/cisco/cisc5-85ceefc9d45f0e54aa610edb5bf476cef73edbc291c9b64fae2405da3f1d4820.png",
-    skills: "",
+    skills: "PROTOTYPING, INTERACTION DESIGN, WIREFRAMING, VISUAL DESIGN, USABILITY RESEARCH",
   },
   {
     title: "Cisco",
-    link: "/",
-    description: "d",
+    link: "/cisco",
+    description: "Cisco's collaboration design team is tackling the future of business-place communication. Designing for telepresence, conferencing, and phone systems were the primary responsibility of this team at Cisco. We aspired to provide the best experience for enterprise collaboration and making the ease of sharing ideas effortless.",
     image:
       "/assets/design/cisco/cisc5-85ceefc9d45f0e54aa610edb5bf476cef73edbc291c9b64fae2405da3f1d4820.png",
-    skills: "",
+    skills: "VISUAL DESIGN, INTERACTION DESIGN ",
   },
   {
     title: "Blend",
-    link: "/",
-    description: "d",
+    link: "/blend",
+    description: "Blend's mission is to bring simplicity and transparency to consumer lending.",
     image:
       "/assets/design/cisco/cisc5-85ceefc9d45f0e54aa610edb5bf476cef73edbc291c9b64fae2405da3f1d4820.png",
-    skills: "",
+    skills: "PRODUCT DESIGN, UX DESIGN DATA, VISUALIZATION",
   },
   {
     title: "Reduct",
-    link: "/",
-    description: "d",
+    link: "/reduct",
+    description: "Reduct.Video is a video editing & transcription startup in San Francisco. With Reduct, editing video is as easy as writing a text document. Highlight, cut, and edit text to watch as your video automatically does the same to the video.",
     image:
       "/assets/design/cisco/cisc5-85ceefc9d45f0e54aa610edb5bf476cef73edbc291c9b64fae2405da3f1d4820.png",
-    skills: "",
+    skills: "VISUAL DESIGN, INTERACTION DESIGN, PRODUCT DESIGN",
   },
   {
     title: "Thesis",
-    link: "/",
-    description: "d",
+    link: "/thesis",
+    description: "Interfaces For Learning was a year long research project investigating the ways we learn online. There is an abundance of online resources for learning these days, but they often blur between education and entertainment. In this year-long study, I surveyed existing literature on personal knowledge bases, information storage techniques, and learning theory; interviewed potential users and experts on the subject; created numerous prototypes, both physical and digital; and collaborated directly with Alcamy.org, an online learning platform founded in 2016, to improve their system based on my research insights and experiments.",
     image:
       "/assets/design/cisco/cisc5-85ceefc9d45f0e54aa610edb5bf476cef73edbc291c9b64fae2405da3f1d4820.png",
-    skills: "",
+    skills: "INTERACTION, DESIGN VISUAL, DESIGN RESEARCH, Prototyping",
   },
   {
-    title: "Fuse Project",
-    link: "/",
-    description: "d",
+    title: "fuseproject",
+    link: "/fuseproject",
+    description: "Experience Design internship during the summer of 2017 for the consumer product firm fuseproject. Spent 3 months working on both physical and digital prototypes for a smart home fitness startup in the Bay Area.",
     image:
       "/public/assets/design/cisco/cisc5-85ceefc9d45f0e54aa610edb5bf476cef73edbc291c9b64fae2405da3f1d4820.png",
-    skills: "",
+    skills: "UX DESIGN, VISUAL DESIGN ",
   },
   {
     title: "Infographic",
-    link: "/",
-    description: "d",
+    link: "/roadtrip",
+    description: "Week-long project to make an infographic describing what interaction design is to non-interaction designers. Made during my Sophomore year of college.",
     image:
       "/public/assets/design/cisco/cisc5-85ceefc9d45f0e54aa610edb5bf476cef73edbc291c9b64fae2405da3f1d4820.png",
-    skills: "",
+    skills: "VISUAL DESIGN, DATA VISUALIZATION, ILLUSTRATION",
   },
 ];
 
